656b311baaa97e28a011152bb1be6bdbd49fad7d
Enzo <61050691+ziboss@users.noreply.github.com>
2020-02-14 22:53:02 +0100

    Update AntenneDirective.java

--- AntenneDirective.java
+++ AntenneDirective.java
@@ -1,5 +1,4 @@
-public class AntenneDirective extends Antennes {
-	
+public class AntenneDirective extends Antenne {
 	super();
 	
 	float orientation;
@@ -9,11 +8,26 @@ public class AntenneDirective extends Antennes {
 		this.orientation = orientation;
 	}
 	
-	public float setOuverture (float ouverture){
-		this.ouverture = ouverture;
+	public float setOuverture (float ouvertureA){
+		this.ouverture = ouvertureA;
 	}
 	
-	public boolean comAntenne(){
+	public double calculGain(float ouverture) {
+		float x = (180 - ouverture);
+		float calc = 6 + x * (34/175); 
+		return calc; //retourne la valeur du gain en fonction de la valeur de l'ouverture
+	}
+	
+	public boolean comAntenne(double distAB, float sensibiliteB, float puissanceA, float ouvertureA, float ouvertureB, float polarisationA, float frequence){
+	
 		//calcul en fonction de sensibilité, distance et perte de DB
+		float lambda = (3*10^8) / frequence; //lambda = c/f
+		double pr = puissanceA + 20 * Math.log(lambda/(4*Math.PI*distAB)) + calculGain(ouvertureA) + calculGain(ouvertureB) + polarisationA; //formule du bilan de liaison
+		
+		if(pr >= sensibiliteB) { //comparaison puissance de reception / sensibilité
+			return true;
+		}else {
+			return false;
+		}
 	}
 }
