cacf5580798feb5f1af441fd59b8edc3e95807bc
ziboss <61050691+ziboss@users.noreply.github.com>
2020-02-14 22:51:16 +0100

    Update AntenneOmnidirectionnelle.java

--- AntenneOmnidirectionnelle.java
+++ AntenneOmnidirectionnelle.java
@@ -1,6 +1,8 @@
 public class AntenneOmnidirectionnelle extends Antenne {
 	super(); //on récupère les caractéristiques de la classe Antenne
 	
+	public float ouverture = 180; //au cas ou une antenne directive doit calculer si elle peut communiquer avec une antenne omnidirectionnelle
+	
 	public boolean comAntenne(double distAB, float sensibiliteB, float puissanceA, float gainA, float gainB, float polarisationA, float frequence){
 
 	float lambda = (3*10^8) / frequence; //lambda = c/f
