effd7f01ba8d87a10229f1aedab700d44c1c41e5
Enzo <61050691+ziboss@users.noreply.github.com>
2020-02-16 21:25:28 +0100

    Update AntenneOmnidirectionnelle.java

--- AntenneOmnidirectionnelle.java
+++ AntenneOmnidirectionnelle.java
@@ -6,7 +6,7 @@ public class AntenneOmnidirectionnelle extends Antenne {
 	public boolean comAntenne(double distAB, float sensibiliteB, float puissanceA, float gainA, float gainB, float polarisationA, float frequence){
 
 	float lambda = (3*10^8) / frequence; //lambda = c/f
-	double pr = puissanceA + 20 * Math.log(lambda/4*Math.PI*distAB) + gainA + gainB + polarisationA; //formule du bilan de liaison
+	double pr = puissanceA + 20 * Math.log(lambda/4*Math.PI*distAB) + gainA + AntenneDirective.calculGain(ouverture) + polarisationA; //formule du bilan de liaison
 	
 	if(pr >= sensibiliteB) { //comparaison puissance de reception / sensibilité
 		return true;
