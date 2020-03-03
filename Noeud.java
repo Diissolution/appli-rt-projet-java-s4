9143a51c3f47e9c3dcc001124fa2d6fed8c6e771
Guillaume2304 <61050742+Guillaume2304@users.noreply.github.com>
2020-03-03 09:56:26 +0100

    Add files via upload

---
+++ Noeud.java
@@ -0,0 +1,39 @@
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class Noeud {
+	
+	//constantes
+    public static final String DEF="0";
+
+    //variables
+    public String idNoeud;
+	public ArrayList pylonesConnect;
+	public HashMap listeAntennes;
+	
+	//constructeur par défaut
+	public Noeud(){
+		this.idNoeud=DEF;
+		this.pylonesConnect= new ArrayList<String>();
+	}
+	
+	//constructeur
+	public Noeud(String idNoeud, ArrayList pylonesConnect){
+		this.idNoeud=idNoeud;
+		this.pylonesConnect=pylonesConnect;
+	}
+
+	
+	public String listeAntennes(String listeAntennes){
+		return listeAntennes;
+	}
+	
+
+	public HashMap caractéristiqueAntenne(){
+		HashMap<String, String> liste = new HashMap<String, String>(); 
+		liste.put("idNoeud", idNoeud);
+		return liste;
+	}
+}
+
