9d7d22dced45d296ef3c8008f68eeb08c84cd6cf
Enzo <61050691+ziboss@users.noreply.github.com>
2020-03-03 09:47:48 +0100

    Update AntenneOmnidirectionnelle.java

--- AntenneOmnidirectionnelle.java
+++ AntenneOmnidirectionnelle.java
@@ -1,18 +1,40 @@
 public class AntenneOmnidirectionnelle extends Antenne {
-	super(); //on récupère les caractéristiques de la classe Antenne
-	
-	public float ouverture = 180; //au cas ou une antenne directive doit calculer si elle peut communiquer avec une antenne omnidirectionnelle
-	
-	public boolean comAntenne(double distAB, float sensibiliteB, float puissanceA, float gainA, float gainB, float polarisationA, float frequence){
 
-	float lambda = (3*10^8) / frequence; //lambda = c/f
-	double pr = puissanceA + 20 * Math.log(lambda/4*Math.PI*distAB) + gainA + AntenneDirective.calculGain(ouverture) + polarisationA; //formule du bilan de liaison
-	
-	if(pr >= sensibiliteB) { //comparaison puissance de reception / sensibilité
-		return true;
-	}else {
-		return false;
+	public AntenneOmnidirectionnelle(){
+		super();
 	}
+	
+	public int calculPolarisation(String polarisationA, String polarisationB){
+	int polarisationAB;
+		
+	if (polarisationA == polarisationB){
+		polarisationAB = 0;
+		
+	}else if( (polarisationA == "horizontale" || polarisationA == "verticale") && (polarisationB == "gauche" || polarisationB == "droite") ){
+		polarisationAB = 3;
+		
+	}else if( (polarisationA == "gauche" || polarisationA == "droite") && (polarisationB == "horizontale" || polarisationB == "verticale") ){
+		polarisationAB = 3;
+			
+	}else{
+		polarisationAB = -30;
+	}
+		
+	return polarisationAB;
+	}
+	
+	public boolean comAntenne(double distAB, float sensibiliteB, float puissanceA, float gainA, float gainB, String polarisationA, String polarisationB, float frequence){
+
+		float lambda = (3*10^8) / frequence; //lambda = c/f
+		double pr = puissanceA + 20 * Math.log(lambda/4*Math.PI*distAB) + gainA + gainB + calculPolarisation(polarisationA,polarisationB); //formule du bilan de liaison
+		
+	//voir doc pour comprend polarisation : http://iutsa.unice.fr/~mgautero/ext/dut/M4210/DocAntennes.pdf
+	
+		if(pr >= sensibiliteB) { //comparaison puissance de reception / sensibilité
+			return true;
+		}else {
+			return false;
+		}
 	
 	}
 }
