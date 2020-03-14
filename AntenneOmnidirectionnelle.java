e379c0a5945cc2a0d680c375f09cd4829019c561
Enzo <61050691+ziboss@users.noreply.github.com>
2020-03-14 18:51:54 +0100

    Update AntenneOmnidirectionnelle.java

--- AntenneOmnidirectionnelle.java
+++ AntenneOmnidirectionnelle.java
@@ -26,21 +26,39 @@ public class AntenneOmnidirectionnelle extends Antenne {
 	public boolean comAntenne(double distAB, float sensibiliteB, float puissanceA, float gainA, float gainB, String polarisationA, String polarisationB, float frequence, float ouvertureB){
 		
 		if (ouvertureB == "NULL") //différencie si la 2eme antenne est directive ou omnidirectionnelle
-			float gainB = gainB;
+			float gainB1 = gainB;
 		}else{
-			float gainB = calculGain(ouvertureB);
+			float gainB1 = calculGain(ouvertureB);
 		}
 		
 		
 		float lambda = (3*10^8) / frequence; //lambda = c/f
-		double pr = puissanceA + 20 * Math.log(lambda/4*Math.PI*distAB) + gainA + gainB + calculPolarisation(polarisationA,polarisationB); //formule du bilan de liaison
+		double pr = puissanceA + 20 * Math.log(lambda/4*Math.PI*distAB) + gainA + gainB1 + calculPolarisation(polarisationA,polarisationB); //formule du bilan de liaison
 		
 	//voir doc pour comprend polarisation : http://iutsa.unice.fr/~mgautero/ext/dut/M4210/DocAntennes.pdf
 	
-		if(pr >= sensibiliteB) { //comparaison puissance de reception / sensibilité
-			return true;
-		}else {
-			return false;
+		double anglPylA= ConvDist.angle(double lat_a, double lon_a, double lat_b, double lon_b); //angle entre les 2 pylones
+		double ouvA = ouvertureA / 2
+		double anglSupA = orientationA + ouvA
+		double anglInfA = orientationA - ouvA
+		if ( anglPylA <= anglSupA && anglPylA >= anglInfA ){
+			double anglPylB = ConvDist.angle(double lat_b, double lon_b, double lat_a, double lon_a); //angle entre les 2 pylones
+			double ouvB = ouvertureB / 2
+			double anglSupB = orientationB + ouvB
+			double anglInfB = orientationB - ouvB
+			if ( anglPylB <= anglSupB && anglPylB >= anglInfB ){
+				if(pr >= sensibiliteB) { //comparaison puissance de reception / sensibilité
+					return true;
+				}else {
+					return false;
+				}
+	
+			}else{
+				return false
+			}
+	
+		}else{
+			return false
 		}
 	
 	}
