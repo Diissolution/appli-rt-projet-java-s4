5c2de5720b35419ab03233bb2f4d05e523e19f1b
Sir-Godfroy <61050687+Sir-Godfroy@users.noreply.github.com>
2020-03-20 09:34:50 +0100

    Create TestAntenne.java

---
+++ TestAntenne.java
@@ -0,0 +1,11 @@
+public class TestAntenne {
+	
+	
+	
+	
+	 public static void main (String args[]){
+		 Antenne a1 = new Antenne();
+		 a1.modifier();
+		 System.out.println(a1.caracteristiqueAntenne());
+	 }
+}
