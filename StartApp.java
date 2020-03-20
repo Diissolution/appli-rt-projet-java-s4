3bc9da4ee9d0ea5e3a12280bd22657a9c6cc743e
Guillaume2304 <61050742+Guillaume2304@users.noreply.github.com>
2020-03-20 09:33:47 +0100

    Add files via upload

--- StartApp.java
+++ StartApp.java
@@ -1,6 +1,35 @@
 
-public class StartApp {
-	 public static void main (String args[]){
-	 
+import java.util.Scanner;
+
+public class StartApp {	
+	Scanner sc = new Scanner(System.in);
+	public void creerNoeud() {
+	
+			  }		 
+	
+	public void addPyloneConnect() {
+		System.out.println("Veuillez saisir l'ID du noeud auquel ajouter :");
+		System.out.println("Veuillez saisir l'ID du pylone :");
+		String idNoeud = sc.nextLine();
+		String idPylone = sc.nextLine();
+		System.out.println("Le pylone " + pylone + " est désormais connecté à ce noeud.");
+	}	
+	
+	public static void main (String args[]){		 
+		
+		System.out.println("Que voulez-vous faire ?");
+		System.out.println("1. Créer un noeud");
+		System.out.println("2. Ajouter un pylone à un Noeud");
+			
+			int choix = sc.nextInt();
+			if(choix ==1){ //création d'un Noeud
+				creerNoeud()
+				}			 
+			if(choix ==2){ //ajout d'un pylone au Noeud
+				
+				}
+					
+	
+		
 	 }
 }
