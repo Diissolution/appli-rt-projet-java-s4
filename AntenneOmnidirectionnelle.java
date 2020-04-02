49b5e1a2d918b9e98044a0f4077e3243ebaefb9f
Enzo <61050691+ziboss@users.noreply.github.com>
2020-04-02 17:06:18 +0200

    Update AntenneOmnidirectionnelle.java

--- AntenneOmnidirectionnelle.java
+++ AntenneOmnidirectionnelle.java
@@ -40,26 +40,37 @@ public class AntenneOmnidirectionnelle extends Antenne {
 		
 		
 		float lambda = (3*10^8) / frequence; //lambda = c/f
-		double pr = puissanceA + 20 * Math.log(lambda/4*Math.PI*distAB) + gainA + gainB1 + calculPolarisation(polarisationA,polarisationB); //formule du bilan de liaison
-		
+		double pr = puissanceA + 20 * Math.log(lambda/(4*Math.PI*distAB)) + gainA + gainB1 + calculPolarisation(polarisationA,polarisationB); //formule du bilan de liaison		
 	//voir doc pour comprend polarisation : http://iutsa.unice.fr/~mgautero/ext/dut/M4210/DocAntennes.pdf
 	
-ConvDist anglPyl = new ConvDist();//angle entre les 2 pylones
+		ConvDist anglPyl = new ConvDist();//angle entre les 2 pylones
 		
-		double anglPylA = anglPyl.angle(lat_b,lon_b,lat_a,lon_a);
+		double anglPylA = anglPyl.angle(lat_a,lon_a,lat_b,lon_b);
+		//System.out.println("valeur de l'angle entre A et B = "+anglPylA);
 		double ouvA = ouvertureA / 2;
+		//System.out.println("valeur de l'ouverture/2 A = "+ouvA);
 		double anglSupA = orientationA + ouvA;
+		//System.out.println("valeur de l'angle superieur A = "+anglSupA);
 		double anglInfA = orientationA - ouvA;
+		//System.out.println("valeur de l'angle inferieur A = "+anglInfA);
 		if ( anglPylA <= anglSupA && anglPylA >= anglInfA ){
 			double anglPylB = anglPyl.angle(lat_b,lon_b,lat_a,lon_a); //angle entre les 2 pylones
+			//System.out.println("valeur de l'angle entre B et A = "+anglPylB);
 			double ouvB = ouvertureB / 2;
+			//System.out.println("valeur de l'ouverture/2 B = "+ouvB);
 			double anglSupB = orientationB + ouvB;
+			//System.out.println("valeur de l'angle superieur B = "+anglSupB);
 			double anglInfB = orientationB - ouvB;
+			//System.out.println("valeur de l'angle inferieur B = "+anglInfB);
 			if ( anglPylB <= anglSupB && anglPylB >= anglInfB ){
 				if(pr >= sensibiliteB) { //comparaison puissance de reception / sensibilité
+					System.out.println("Puissance d'émission = "+pr);
+					System.out.println("la sensibilité = "+sensibiliteB);
 					return true;
 				}else {
 					System.out.println("Puissance d'émission plus faible que la sensibilité");
+					System.out.println("Puissance d'émission = "+pr);
+					System.out.println("la sensibilité = "+sensibiliteB);
 					return false;
 				}
 	
@@ -75,4 +86,5 @@ ConvDist anglPyl = new ConvDist();//angle entre les 2 pylones
 	
 	}
 }
+
 
