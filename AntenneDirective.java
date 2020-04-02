536d1a45155529a52dd51d5bce621ee5f6b08d1c
Enzo <61050691+ziboss@users.noreply.github.com>
2020-04-02 17:05:20 +0200

    Update AntenneDirective.java

--- AntenneDirective.java
+++ AntenneDirective.java
@@ -1,6 +1,7 @@
 
 public class AntenneDirective extends Antenne {
 	
+	ConvDist convDist = new ConvDist();
 	float orientation;
 	float ouverture;
 	
@@ -18,8 +19,12 @@ public class AntenneDirective extends Antenne {
 	
 	public double calculGain(float ouverture) { //methode calcul de gain
 		float x = (180 - ouverture);
-		float calc = 6 + x * (34/175); 
-		return calc; //retourne la valeur du gain en fonction de la valeur de l'ouverture
+		//System.out.println("valeur de x = "+x);
+		//System.out.println("valeur de (x * 34)/175 = "+ (x*34)/175);
+		float calc1 = convDist.convertChiffreVirgule((x * 34)/175);
+		//System.out.println("valeur de multi = "+calc1);
+		//System.out.println("valeur de calc = "+calc);
+		return 6 + calc1; //retourne la valeur du gain en fonction de la valeur de l'ouverture
 	}
 	
 	public int calculPolarisation(String polarisationA, String polarisationB){
@@ -41,7 +46,7 @@ public class AntenneDirective extends Antenne {
 		return polarisationAB;
 	}
 	
-	public boolean comAntenne(double distAB, float sensibiliteB, float puissanceA, float ouvertureA, float ouvertureB, String polarisationA, String polarisationB, float frequence, float gainB, float orientationA, float orientationB,
+	public boolean comAntenne(double distAB, float sensibiliteB, float puissanceA, float ouvertureA, float ouvertureB, String polarisationA, String polarisationB, double frequence, float gainB, float orientationA, float orientationB,
 			double lat_a, double lon_a, double lat_b, double lon_b){
 		
 		double gainB1;
@@ -56,28 +61,49 @@ public class AntenneDirective extends Antenne {
 		
 		//calcul en fonction de sensibilité, distance et perte de DB
 		
-		float lambda = (3*10^8) / frequence; //lambda = c/f
+		double lambda = (3*Math.pow(10,8)) / frequence; //lambda = c/f
+		//System.out.println("valeur lambda = "+lambda);
 		double pr = puissanceA + 20 * Math.log(lambda/(4*Math.PI*distAB)) + calculGain(ouvertureA) + gainB1 + calculPolarisation(polarisationA,polarisationB); //formule du bilan de liaison
-		
+		//double pr2 = (calculGain(ouvertureA) * gainB1 * (lambda/(4*Math.PI*distAB)) * (lambda/(4*Math.PI*distAB))) * puissanceA;
+		//System.out.println("valeur puissance emissions = "+pr);
+		//System.out.println("valeur puissance A = "+puissanceA);
+		//System.out.println("valeur (lambda/(4*Math.PI*distAB)) = "+(lambda/(4*Math.PI*distAB)));
+		//System.out.println("valeur dist = "+distAB);
+		//System.out.println("valeur puissance reception site = "+pr2);
+		//System.out.println("valeur de gainB1 = "+gainB1);
+		//System.out.println("valeur gainA (calc) = "+calculGain(ouvertureA));
+		//System.out.println("valeur calculPolarisation = "+ calculPolarisation(polarisationA,polarisationB));
 		//voir doc pour comprend polarisation : http://iutsa.unice.fr/~mgautero/ext/dut/M4210/DocAntennes.pdf
 		
 
 		ConvDist anglPyl = new ConvDist();//angle entre les 2 pylones
 		
-		double anglPylA = anglPyl.angle(lat_b,lon_b,lat_a,lon_a);
+		double anglPylA = anglPyl.angle(lat_a,lon_a,lat_b,lon_b);
+		//System.out.println("valeur de l'angle entre A et B = "+anglPylA);
 		double ouvA = ouvertureA / 2;
+		//System.out.println("valeur de l'ouverture/2 A = "+ouvA);
 		double anglSupA = orientationA + ouvA;
+		//System.out.println("valeur de l'angle superieur A = "+anglSupA);
 		double anglInfA = orientationA - ouvA;
+		//System.out.println("valeur de l'angle inferieur A = "+anglInfA);
 		if ( anglPylA <= anglSupA && anglPylA >= anglInfA ){
 			double anglPylB = anglPyl.angle(lat_b,lon_b,lat_a,lon_a); //angle entre les 2 pylones
+			//System.out.println("valeur de l'angle entre B et A = "+anglPylB);
 			double ouvB = ouvertureB / 2;
+			//System.out.println("valeur de l'ouverture/2 B = "+ouvB);
 			double anglSupB = orientationB + ouvB;
+			//System.out.println("valeur de l'angle superieur B = "+anglSupB);
 			double anglInfB = orientationB - ouvB;
+			//System.out.println("valeur de l'angle inferieur B = "+anglInfB);
 			if ( anglPylB <= anglSupB && anglPylB >= anglInfB ){
 				if(pr >= sensibiliteB) { //comparaison puissance de reception / sensibilité
+					System.out.println("Puissance d'émission = "+pr);
+					System.out.println("la sensibilité = "+sensibiliteB);
 					return true;
 				}else {
 					System.out.println("Puissance d'émission plus faible que la sensibilité");
+					System.out.println("Puissance d'émission = "+pr);
+					System.out.println("la sensibilité = "+sensibiliteB);
 					return false;
 				}
 	
